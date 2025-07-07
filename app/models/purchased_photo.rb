45ab5851a4894fda753209fcb1c2648692793f97
flosteno <florian-benoit@outlook.fr>
2025-07-07 17:22:22 +0200

    Add associations and validations to PurchasedPhoto model

--- app/models/purchased_photo.rb
+++ app/models/purchased_photo.rb
@@ -1,4 +1,8 @@
 class PurchasedPhoto < ApplicationRecord
+   # === Associations ===
   belongs_to :user
   belongs_to :photo
+
+  # === Validations ===
+  validates :photo_id, uniqueness: { scope: :user_id, message: "has already been purchased by this user" }
 end
