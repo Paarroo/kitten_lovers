95b5f747188ac326e17c0ae24a63dea07189d74a
Paarroo <code_source_0@protonmail.com>
2025-07-07 22:17:07 +0200

    suppadmin + purchassed_items + addin item_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,17 @@
 class User < ApplicationRecord
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_one :cart, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :purchased_items, dependent: :destroy
+   has_many :purchased_photos, through: :purchased_items, source: :photo
 
   def admin?
     is_admin
   end
-end
 
+  def has_purchased?(photo)
+     purchased_items.exists?(photo: photo)
+   end
+end
