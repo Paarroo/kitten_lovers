58c5d7ec44e7ca43ac28f1061e52e72786b38d6d
Drixyz13 <mattmariller@gmail.com>
2025-07-07 15:22:47 +0200

    Fix global add routesand root create home controller with method index

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,14 @@ Rails.application.routes.draw do
   resources :admins
   devise_for :users
   resources :users
-
   resources :cart_items
   resources :carts
+  resources :photos
+  resources :orders
+  resources :order_items
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Route racine
+  root "home#index"
 end
