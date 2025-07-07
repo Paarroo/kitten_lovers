b00dac3811c1abef2a400b1fa397e1384ecadc88
flosteno <florian-benoit@outlook.fr>
2025-07-07 19:09:12 +0200

    Add associations and validations of Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  # === Associations ===
+  has_many :cart_items, dependent: :destroy
+  has_many :order_items, dependent: :destroy
+  has_many :purchased_items, dependent: :destroy
+
+  # === Validations ===
+  validates :title, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_url, presence: true
 end
