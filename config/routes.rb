21b6b8c3eadb2acdae06f46d066a3621d550f807
Paarroo <code_source_0@protonmail.com>
2025-07-08 12:37:16 +0200

    application.rb + routes + devise_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users
+ devise_for :users
 
+ # avo_admin
+ authenticate :user, ->(user) { user.admin? } do
+   mount Avo::Engine, at: Avo.configuration.root_path
+ end
 
-  authenticate :user, ->(user) { user.admin? } do
-    mount Avo::Engine, at: Avo.configuration.root_path
-  end
+ resources :items, only: [ :index, :show ]
 
+ authenticate :user do
+   resource :profile, controller: 'users', only: [ :show, :edit, :update ]
 
-  resources :users
-  resources :items
-  resources :carts
-  resources :cart_items
-  resources :orders, only: [ :index, :show, :create ]
-  resources :order_items, only: [ :show ]
-  resources :purchased_items, only: [ :index ]
+   resources :carts, only: [ :show, :update ]
+   resources :cart_items, only: [ :create, :update, :destroy ]
+   resources :orders, only: [ :index, :show, :create ]
+   resources :order_items, only: [ :show ]
+   resources :purchased_items, only: [ :index ]
+ end
 
+ get "up" => "rails/health#show", as: :rails_health_check
 
-  # optionnal for Avo
-  namespace :admin do
-    resources :items
-    resources :users
-    resources :orders
-  end
-
-  # survey
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  root "items#index"
+ root "items#index"
 end
