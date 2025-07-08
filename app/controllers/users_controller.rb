4d49bd24771d3a7baf09c9cea5747fc34cde28ab
Paarroo <code_source_0@protonmail.com>
2025-07-08 12:46:45 +0200

    add more controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [ :show, :edit, :update ]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to profile_path, notice: "Profil mis à jour avec succès."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description)
+  end
+
+  def ensure_correct_user
+    @user = current_user
+    # Users can only access their own profile
+  end
+end
