9183bf1fe9db8352253eebbbf1dc41f01b87cd22
Paarroo <code_source_0@protonmail.com>
2025-07-08 23:51:17 +0200

    config/import + hello_controller.js

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    console.log(" Hello controller connected successfully!")
+    console.log(" Stimulus is working!")
   }
+}
+
+// Register it manually for testing
+if (window.Stimulus) {
+  window.Stimulus.register("hello", class extends Controller {
+    connect() {
+      console.log(" Hello controller connected via window.Stimulus!")
+    }
+  })
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,16 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
+# Bootstrap
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
 
-pin "application"
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+# Explicitly pin Stimulus controllers
+pin "controllers/sessions_controller", to: "controllers/sessions_controller.js"
+pin "controllers/registrations_controller", to: "controllers/registrations_controller.js"
+pin "controllers/passwords_controller", to: "controllers/passwords_controller.js"
